926b470cf8eb2c954ccbcad0d7e67d7945e7548e
Wovi10 <wout.vinck@gmail.com>
2023-09-06 16:17:55 +0200

    Implemented Command Pattern

---
+++ Command/IEmployeeManagerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Command;
+
+public interface IEmployeeManagerRepository
+{
+    void AddEmployee(int managerId, Employee employee);
+    void RemoveEmployee(int managerId, Employee employee);
+    bool HasEmployee(int managerId, int employeeId);
+    void WriteDataStore();
+}
